919938fda837c8b9a812c6d98bcb282d1b3494de
thekapilpatel <thekapilpatelofficial@gmail.com>
2025-04-29 19:33:01 +0530

    29

--- My-app/src/App.jsx
+++ My-app/src/App.jsx
@@ -8,18 +8,7 @@ const App = () => {
   return (
     <>
 
-      <div className="p-5 d-flex justify-content-center" >
-        <h1>Learingin React Toolkit</h1>
-        <button className="btn btn-primary me-2" onClick={() => dispatch(increment())}>
-          Increment
-        </button> 
-
-        <h1>{mycnt}</h1> 
-
-        <button className="btn btn-danger" onClick={() => dispatch(decrement())}>
-          Decrement
-        </button>
-      </div>
+     
     </>
   );
 };
